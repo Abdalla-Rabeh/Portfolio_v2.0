d5f44f5c4667b98d4eef2bd5641a6b5bb5bb2d4c
Abdalla-Rabeh <abdallahcodeing@gmail.com>
2022-11-14 20:47:58 +0200

    updata

--- static/js/chat.js
+++ static/js/chat.js
@@ -2,9 +2,13 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/6370da66daff0e1306d728a3/1ghoda8ug';
+s1.src='https://embed.tawk.to/63728bd8daff0e1306d75e3a/1ghrn4i3i';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+
+
+
+
 
